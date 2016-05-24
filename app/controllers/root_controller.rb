a21456d14b1b424d7dc5d6c058667c6f96616cc5
Andrew Fader <andrevan@gmail.com>
2016-05-24 21:40:38 +0000

    latlng

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,5 +2,6 @@ include Geokit::Geocoders
 class RootController < ApplicationController
   def index
     @location = MultiGeocoder.geocode(request.ip).city
+    @lat_lng = cookies[:lat_lng].split("|")
   end
 end
