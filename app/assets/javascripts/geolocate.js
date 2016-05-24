10b4b4409d20aa99e989bc23dd5b2f0ffbdabe6f
Andrew Fader <fader@yagni.co>
2016-05-24 18:15:59 -0400

    latlng

--- app/assets/javascripts/geolocate.js
+++ app/assets/javascripts/geolocate.js
@@ -1,10 +1,9 @@
-navvigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
+navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
 function locationSuccess(position) {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
-  alert('yes')
+  var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
+  document.cookie = "lat_lng=" + escape(cookie_val);
 }
 
 function locationFail() {
-  alert(‘Oops, could not find you.’);
+  alert('Oops, could not find you.');
 }
